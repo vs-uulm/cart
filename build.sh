50f3201ba99c6f29000c7345695142d5f22463b6
Alexander Heß <alexander.hess@uni-ulm.de>
2024-10-05 18:07:22 +0200

    Remove the --no-cache option

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Building crypto library"
-docker buildx build --platform linux/amd64 --no-cache -f docker/crypto/Dockerfile -t cart-crypto:latest .
+docker buildx build --platform linux/amd64 -f docker/crypto/Dockerfile -t cart-crypto:latest .
 
 echo "Building the cart and bft-smart libraries"
-docker buildx build --platform linux/amd64 --no-cache -f docker/build/Dockerfile -t cart-java-builder:latest .
+docker buildx build --platform linux/amd64 -f docker/build/Dockerfile -t cart-java-builder:latest .
 
 echo "Assembling the server container"
-docker buildx build --platform linux/amd64 --no-cache -f docker/server/Dockerfile -t cart-monolith:latest .
+docker buildx build --platform linux/amd64 -f docker/server/Dockerfile -t cart-monolith:latest .
 
 echo "Assembling the client container"
-docker buildx build --platform linux/amd64 --no-cache -f docker/client/Dockerfile -t cart-client:latest .
+docker buildx build --platform linux/amd64 -f docker/client/Dockerfile -t cart-client:latest .
