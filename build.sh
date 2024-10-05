2a0edd8e5aeeefae669265c0fafa8a3fe6325c9d
Alexander Heß <alexander.hess@uni-ulm.de>
2024-10-05 18:10:54 +0200

    Add missing Dockerfile

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo "Building crypto library"
 docker buildx build --platform linux/amd64 -f docker/crypto/Dockerfile -t cart-crypto:latest .
 
 echo "Building the cart and bft-smart libraries"
-docker buildx build --platform linux/amd64 -f docker/build/Dockerfile -t cart-java-builder:latest .
+docker buildx build --platform linux/amd64 -f docker/java/Dockerfile -t cart-java-builder:latest .
 
 echo "Assembling the server container"
 docker buildx build --platform linux/amd64 -f docker/server/Dockerfile -t cart-monolith:latest .
